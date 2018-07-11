0294ed0e0d28a9af2c5d2b862aae753c7f47a148
kingflamez <flamekeed@gmail.com>
2018-07-11 17:46:50 +0100

    formatted responses

--- Components/RaveCard.js
+++ Components/RaveCard.js
@@ -1,53 +1,53 @@
 import RavePayment from "../Core/RavePaymentBase"
 import RaveApi from "../RaveApi"
-export default class Card extends RavePayment{
-    constructor(publicKey, secretKey, production=false){
+export default class Card extends RavePayment {
+    constructor(publicKey, secretKey, production = false) {
         super(publicKey, secretKey, production);
     }
 
-    charge(payload){
+    charge(payload) {
         let endpoint = this.baseUrl + this.endpointMap["card"]["charge"];
-        if(!("txRef" in payload))
+        if (!("txRef" in payload))
             payload["txRef"] = RaveApi.generateTransactionReference();
 
         return super.charge(payload, endpoint);
     }
 
-    preauth(payload){
+    preauth(payload) {
         let endpoint = this.baseUrl + this.endpointMap["card"]["charge"];
         // if transaction is not defined or not set properly
-        if(!("txRef" in payload))
+        if (!("txRef" in payload))
             payload["txRef"] = RaveApi.generateTransactionReference();
-        
+
         // adds charge_type if it is not defined or not set correctly
-        if(!("charge_type" in payload) || payload["charge_type"] !== "preauth")
+        if (!("charge_type" in payload) || payload["charge_type"] !== "preauth")
             payload["charge_type"] = "preauth"
 
         return super.charge(payload, endpoint);
     }
 
-    validate(otp, flwRef){
+    validate(otp, flwRef) {
         let endpoint = this.baseUrl + this.endpointMap["card"]["validate"];
         return super.validate(otp, flwRef, endpoint);
     }
 
-    handleCharge(responseJson, txRef){
-        if("message" in responseJson && responseJson["message"]=="AUTH_SUGGESTION")
-            return {status: responseJson["status"], authSuggested: responseJson["data"]["suggested_auth"], validationComplete: false, flwRef: null, txRef}
+    handleCharge(responseJson, txRef) {
+        if ("message" in responseJson && responseJson["message"] == "AUTH_SUGGESTION")
+            return { status: responseJson["data"]["status"], authSuggested: responseJson["data"]["suggested_auth"], validationComplete: false, flwRef: null, txRef }
 
-        else if("data" in responseJson && responseJson["data"]["chargeResponseCode"] == "00")
-            return {status: responseJson["status"], authSuggested: null, validationComplete: true, flwRef: responseJson["data"]["flwRef"], txRef}
-        
+        else if ("data" in responseJson && responseJson["data"]["chargeResponseCode"] == "00")
+            return { status: responseJson["data"]["status"], authSuggested: null, validationComplete: true, flwRef: responseJson["data"]["flwRef"], txRef }
+
         else
-            return {status: responseJson["status"], authSuggested: null, validationComplete: false, flwRef: responseJson["data"]["flwRef"], txRef}
+            return { status: responseJson["data"]["status"], authSuggested: null, validationComplete: false, authurl: responseJson["data"]["authurl"], flwRef: responseJson["data"]["flwRef"], txRef }
     }
 
-    handleValidate(responseJson, resolve, reject){
-        if("data" in responseJson && responseJson["data"]["tx"]["chargeResponseCode"] == "00"){
-            resolve({status: responseJson["status"], validationComplete:true, flwRef: responseJson["data"]["tx"]["flwRef"], txRef: responseJson["data"]["tx"]["txRef"]})
+    handleValidate(responseJson, resolve, reject) {
+        if ("data" in responseJson && responseJson["data"]["tx"]["chargeResponseCode"] == "00") {
+            resolve({ status: responseJson["status"], validationComplete: true, flwRef: responseJson["data"]["tx"]["flwRef"], txRef: responseJson["data"]["tx"]["txRef"] })
         }
-        else{
-            reject({status: responseJson["status"], validationComplete:false, flwRef: responseJson["data"]["tx"]["flwRef"], txRef: responseJson["data"]["tx"]["txRef"]});
+        else {
+            reject({ status: responseJson["status"], validationComplete: false, flwRef: responseJson["data"]["tx"]["flwRef"], txRef: responseJson["data"]["tx"]["txRef"] });
         }
     }
 
